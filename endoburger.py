66b8d4c472077ba8935342914e2362e96629d4a1
Grzegorz Mozer <propbono@gmail.com>
2014-12-22 15:42:08 +0100

    Add todo points to implement

---
+++ endoburger.py
@@ -0,0 +1,10 @@
+__author__ = 'propbono'
+
+import requests
+
+#1 get endomondo site
+#2 parse site adn return burned burgers
+#3 create google client
+#4 get proper spreadsheet
+#5 update cells
+#6 return curent values

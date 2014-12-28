dd00079eead499f3b823aa5852b6809ff6abf37f
Grzegorz Mozer <propbono@gmail.com>
2014-12-28 20:31:24 +0100

    Add comments to credentials fields

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 __author__ = 'propbono'
 
-
-ENDOMONDO_USER = ''
-ENDMONDO_PASSWORD = ''
-GOOGLE_USER = ''
-GOOGLE_PASSWORD = ''
+ENDOMONDO_USER = '' # EndomondoApplication_USER from coockies it could be normal user name
+ENDMONDO_PASSWORD = '' # EndomondoApplication_AUTH from coockies it must be the same value like in
+# a coockie
+GOOGLE_USER = '' # Normal user name for google account
+GOOGLE_PASSWORD = '' # Normal password for google account

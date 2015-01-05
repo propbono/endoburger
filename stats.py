ae07315b633b711eac695fdb92dbf4485140d449
Grzegorz Mozer <propbono@gmail.com>
2015-01-05 13:18:51 +0100

    Add Stats class to keep statistic data

---
+++ stats.py
@@ -0,0 +1,13 @@
+class Stats:
+    def __init__(self):
+        self.total_workouts = 0
+        self.total_duration = 0
+        self.total_distance = 0
+        self.trips_around_the_world = 0
+        self.trips_to_the_moon = 0
+        self.calories_burned = 0
+        self.burgers_burned = 0
+        self.time_stats = ''
+
+
+stats = Stats()

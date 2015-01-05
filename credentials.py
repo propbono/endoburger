b5f796fb74917305201bece0baa09312a9ff0452
Grzegorz Mozer <propbono@gmail.com>
2015-01-05 12:26:03 +0100

    Fixed untracked files

--- credentials.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'propbono'
-
-ENDOMONDO_USER = '' # EndomondoApplication_USER from coockies it could be normal user name
-ENDMONDO_PASSWORD = '' # EndomondoApplication_AUTH from coockies it must be the same value like in
-# a coockie
-GOOGLE_USER = '' # Normal user name for google account
-GOOGLE_PASSWORD = '' # Normal password for google account
